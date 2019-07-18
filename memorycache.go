25c925a35a422bc51bed178cbacd6b889634031c
Nur Kutlugallyamov <nurkng@yandex.ru>
2019-07-18 19:50:13 +0500

    start

---
+++ memorycache.go
@@ -0,0 +1,273 @@
+package memorycache
+
+import (
+	"errors"
+	"sync"
+  "os"
+	"time"
+)
+
+//==============================
+
+type Cache struct {
+    sync.RWMutex
+    items                 map[string]Item
+    defaultExpiration     time.Duration
+    cleanupInterval       time.Duration
+    expirationTime        time.Duration
+    itemsSecondCache      map[string]int64
+}
+
+type Item struct {
+    Value                           interface{}
+    Created                         time.Time
+    ExpirationDeleteTime            int64
+    TransferCacheSecondLevelTime    int64
+}
+
+//===============================
+
+func New(defaultExpiration, cleanupInterval, expirationTime time.Duration) *Cache {
+
+	items := make(map[string]Item)
+  itemsSecondCache := make(map[string]int64)
+
+	cache := Cache{
+		items:             items,
+		defaultExpiration: defaultExpiration,
+		cleanupInterval:   cleanupInterval,
+    expirationTime:    expirationTime,
+    itemsSecondCache:  itemsSecondCache,
+	}
+
+  //лог: "make structure"
+
+	if cleanupInterval > 0 {
+		cache.StartGC()
+	}
+
+	return &cache
+}
+
+func (c *Cache) Set(key string, value interface{}, durationDelete, durationTransfer time.Duration) {
+
+	var expirationDeleteTime, transferCacheSecondLevelTime int64
+
+	if durationDelete == 0 {
+		durationDelete = c.expirationTime
+	}
+
+  if durationTransfer == 0 {
+		durationTransfer = c.defaultExpiration
+	}
+
+	if durationDelete > 0 {
+		expirationDeleteTime = time.Now().Add(durationDelete).UnixNano()
+	}
+
+  if durationTransfer > 0 {
+		transferCacheSecondLevelTime = time.Now().Add(durationTransfer).UnixNano()
+	}
+
+	c.Lock()
+
+	c.items[key] = Item{
+		Value:      value,
+    Created:    time.Now(),
+		ExpirationDeleteTime: expirationDeleteTime,
+    TransferCacheSecondLevelTime: transferCacheSecondLevelTime,
+	}
+
+  // лог: "structure "key" is added in RAM"
+
+  c.Unlock()
+
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+
+	c.RLock()
+
+	defer c.RUnlock()
+
+	item, found := c.items[key]
+
+	if !found {
+    item, found := c.GetSecondCache(key)
+    if !found {
+      return nil, false
+    }
+	}
+
+//Ниже проверка на жизнь найденной записи
+	if item.ExpirationDeleteTime > 0 {
+		if time.Now().UnixNano() > item.ExpirationDeleteTime {
+			return nil, false
+		}
+	}
+
+	return item.Value, true
+}
+
+func (c *Cache) GetSecondCache(key string) (interface{}, bool) {
+
+	c.RLock()
+
+	defer c.RUnlock()
+
+	item, found := c.itemsSecondCache[key]
+
+	if !found {
+    return nil, false
+	}
+
+  // Нужно сделать чтение с файла под именем key сруктуры Item в переменную item
+
+  c.items[key] = item
+  //c.Set(key, item.value, item.durationDelete, item.durationTransfer)
+
+  delete(c.itemsSecondCache, key)
+
+  //лог: "structure "key" moveed is HDD in RAM"
+
+	return item.Value, true
+}
+
+func (c *Cache) Delete(key string) error {
+
+	c.Lock()
+
+	defer c.Unlock()
+
+	if _, found := c.items[key]; !found {
+    found := c.deleteSecondCache(key)
+    if !found {
+      return errors.New("Key not found")
+    } else {
+      return nil
+    }
+	}
+
+	delete(c.items, key)
+  //лог: "structure "key" delete"
+
+
+	return nil
+}
+
+func (c *Cache) StartGC() {
+	go c.GC()
+}
+
+func (c *Cache) GC() {
+
+	for {
+
+		<-time.After(c.cleanupInterval)
+
+		if c.items == nil {
+			return
+		}
+
+    if keys := c.transferKeys(); len(keys) != 0 {
+      c.transferItems(keys)
+    }
+
+		if keys := c.expiredKeys(); len(keys) != 0 {
+			c.clearItems(keys)
+		}
+	}
+
+  c.CacheStatus()
+}
+
+func (c *Cache) CacheStatus() {
+
+//лог: "number of structures in RAM: len(c.items)"
+//лог: "number of structures in HDD: len(c.itemsSecondCache)"
+
+}
+
+func (c *Cache) expiredKeys() (keys []string) {
+
+	c.RLock()
+
+	defer c.RUnlock()
+
+	for k, i := range c.items {
+		if time.Now().UnixNano() > i.ExpirationDeleteTime && i.ExpirationDeleteTime > 0 {
+			keys = append(keys, k)
+		}
+	}
+
+  for k, i := range c.itemsSecondCache {
+    if time.Now().UnixNano() > i && i > 0 {
+      keys = append(keys, k)
+    }
+  }
+
+	return
+}
+
+func (c *Cache) transferKeys() (keys []string) {
+
+	c.RLock()
+
+	defer c.RUnlock()
+
+	for k, i := range c.items {
+		if time.Now().UnixNano() > i.TransferCacheSecondLevelTime && i.TransferCacheSecondLevelTime > 0 {
+			keys = append(keys, k)
+		}
+	}
+
+	return
+}
+
+func (c *Cache) clearItems(keys []string) {
+
+	c.Lock()
+
+	for _, k := range keys {
+    err := os.Remove(k)
+    if err != nil {
+      delete(c.items, k)
+    }	else {
+      delete(c.itemsSecondCache, k)
+    }
+	}
+
+  //лог: "structure "key" delete"
+
+  c.Unlock()
+}
+
+func (c *Cache) transferItems(keys []string) {
+
+	c.Lock()
+  c.itemsSecondCache[key] = c.items[key].ExpirationDeleteTime
+
+  //нужно сделать запись в файл с именем key структуры c.items[key]
+
+  delete(c.items, k)
+
+  //лог: "structure "key" moveed is RAM in HDD"
+
+  c.Unlock()
+}
+
+func (c *Cache) deleteSecondCache(key string) (bool) {
+
+	c.Lock()
+
+  defer c.Unlock()
+
+  err := os.Remove(key)
+  if err != nil {
+    return false
+  }
+
+  delete(c.itemsSecondCache, key)
+  //лог: "structure "key" delete"
+  return true
+}
